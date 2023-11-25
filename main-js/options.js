c4a86bccefac252d236e58492b35b44d65c5d2d0
DavidMarquezad <davidmarquezado07@gmail.com>
2023-11-25 11:25:41 -0500

    proyecto

---
+++ main-js/options.js
@@ -0,0 +1,20 @@
+// var ultimaTallaSeleccionada = ""
+
+function seleccionarTalla(talla) {
+    console.log("Talla seleccionada: " + talla);
+}
+
+
+// function obtenerUltimaTallaSeleccionada() {
+//     if (ultimaTallaSeleccionada) {
+//         console.log("Última talla seleccionada: " + ultimaTallaSeleccionada);
+//     } else {
+//         console.log("Ninguna talla seleccionada aún.");
+//     }
+// }   
+
+
+function añadirProducto(producto){
+    console.log("Se añadio " + producto + " al carrito")
+}
+
---
+++ main-js/text.js
@@ -0,0 +1,14 @@
+const typed = new Typed ('.typed', {
+    strings: ['"La misma elegancia, con un toque moderno. Marquez, el estilo que se adapta con el presente."'
+],
+    typeSpeed: 35,
+    startDelay: 90,
+    backSpeed:40,
+    contentType: 'html',
+    shuffle:false,
+    backDelay:2500,
+    loop: true,
+    showCursor:true,
+});
+
+// hacer mas anchas las letras y ver si se puede hacer las lento el inicio 
